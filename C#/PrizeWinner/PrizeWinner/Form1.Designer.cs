88a5cd14ab23e5c9862083f2ab137c4d9b580075
danmashel <danmashel@gmail.com>
2015-09-10 15:20:47 -0400

    First commit

---
+++ C#/PrizeWinner/PrizeWinner/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PrizeWinner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.participantsList = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.pickButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.addTicketButton = new System.Windows.Forms.Button();
+            this.newName = new System.Windows.Forms.TextBox();
+            this.ticketsList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pick Random Winner";
+            // 
+            // participantsList
+            // 
+            this.participantsList.FormattingEnabled = true;
+            this.participantsList.Location = new System.Drawing.Point(12, 37);
+            this.participantsList.Name = "participantsList";
+            this.participantsList.Size = new System.Drawing.Size(200, 21);
+            this.participantsList.TabIndex = 2;
+            this.participantsList.Text = "Choose Participant";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(116, 179);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(104, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add Participant";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // pickButton
+            // 
+            this.pickButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickButton.Location = new System.Drawing.Point(12, 93);
+            this.pickButton.Name = "pickButton";
+            this.pickButton.Size = new System.Drawing.Size(200, 80);
+            this.pickButton.TabIndex = 4;
+            this.pickButton.Text = "Pick random winner";
+            this.pickButton.UseVisualStyleBackColor = true;
+            this.pickButton.Click += new System.EventHandler(this.pickButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(47, 208);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(126, 23);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove Participant";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // addTicketButton
+            // 
+            this.addTicketButton.Location = new System.Drawing.Point(47, 64);
+            this.addTicketButton.Name = "addTicketButton";
+            this.addTicketButton.Size = new System.Drawing.Size(126, 23);
+            this.addTicketButton.TabIndex = 7;
+            this.addTicketButton.Text = "Add ticket to pool";
+            this.addTicketButton.UseVisualStyleBackColor = true;
+            this.addTicketButton.Click += new System.EventHandler(this.addTicketButton_Click);
+            // 
+            // newName
+            // 
+            this.newName.Location = new System.Drawing.Point(10, 179);
+            this.newName.Name = "newName";
+            this.newName.Size = new System.Drawing.Size(100, 20);
+            this.newName.TabIndex = 8;
+            // 
+            // ticketsList
+            // 
+            this.ticketsList.FormattingEnabled = true;
+            this.ticketsList.Location = new System.Drawing.Point(245, 37);
+            this.ticketsList.Name = "ticketsList";
+            this.ticketsList.Size = new System.Drawing.Size(120, 199);
+            this.ticketsList.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ticket Totals";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 250);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ticketsList);
+            this.Controls.Add(this.newName);
+            this.Controls.Add(this.addTicketButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.pickButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.participantsList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Winner Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox participantsList;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button pickButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button addTicketButton;
+        private System.Windows.Forms.TextBox newName;
+        private System.Windows.Forms.ListBox ticketsList;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C#/PrizeWinner/PrizeWinner/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrizeWinner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declare globals class to hold global variables
+        public static class Globals 
+        {
+            public static List<participant> participants = new List<participant>(); //Contains a list of all the participant objects
+            public static List<string> tickets = new List<string>(); //Contains the list of 'tickets' to be randomly selected from
+        }
+
+        //participant object to contain name and ticket totals for individuals
+        public class participant
+        {
+            public string name { get; set; } //public name
+            public int ticketTotal { get; set; } //public ticket total
+        }
+
+        //Runs when form loads
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines("defaultMembers.txt"); //Reads the default members from the defaultMemeber.txt file and stores in an array
+            foreach (string line in lines) //Iterates over each line in the text files
+            {
+                Globals.participants.Add(new participant { name = line, ticketTotal = 0 }); //Adds each line as an object, to the global participants object list from above.
+                participantsList.Items.Add(line); //Adds them to the comboBox
+                refreshTotals(); //Refreshes the names and totals on the right-hand side
+            }
+        }
+
+        //Picks a random winner
+        private void pickButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(); //Creates new Random object
+            int randomNumber = rnd.Next(0, Globals.tickets.Count()); //Generates random number between 0 and the length of the tickets list
+            if (Globals.tickets.Count() > 0) //Only fires if there are actually tickets in the list
+            {
+                string winnerName = Globals.tickets[randomNumber]; //Sets the randomly selected winner's name by plugging in the random number as array index value
+                MessageBox.Show(winnerName + " is the winner!"); //Shows who the winner is
+                Globals.tickets.RemoveAll(item => item == winnerName); //Removes all entries in tickets where the name matches the winner (so they cant win again)
+            }
+            else
+            {
+                MessageBox.Show("There are no more eligible people to win"); //If nobody is in the ticket list that can win
+            }
+            
+        }
+
+        //Refresh right-hand side with name and totals
+        public void refreshTotals(){ 
+            ticketsList.Items.Clear(); //Clears the whole list, which will be refreshed.
+            foreach (participant part in Globals.participants)//Iterates over each participant object in the global participants list with the temporary value 'part'
+            { 
+                if (part.name == participantsList.Text){ //checks to see if the name it's on matches the selected participant
+                    part.ticketTotal = part.ticketTotal + 1; //if it matches, it increases the ticket total by 1                 
+                }
+                ticketsList.Items.Add(part.name + "(" + part.ticketTotal.ToString() + ")"); //Creates the actual string to be placed in the right-hand size
+            }
+        }
+
+        //Adds a ticket for selected participant
+        private void addTicketButton_Click(object sender, EventArgs e)
+        {
+            if (participantsList.SelectedIndex >= 0) //Checks to see if you actually selected a participant
+            {
+                refreshTotals(); //Calls the refresh totals method
+                Globals.tickets.Add(participantsList.Text); //Adds the name to the global tickets list
+            }
+        }
+
+        //Add a new participant to existing. Does not update default text
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (newName.Text != null) //Checks to make sure you've actually entered something
+            {
+                Globals.participants.Add(new participant { name = newName.Text, ticketTotal = 0 }); //adds object to global participants list
+                participantsList.Items.Add(newName.Text); //adds name to drop-down comboBox
+                MessageBox.Show("You have successfully added " + newName.Text + " to the list"); //Tells you it added it
+                refreshTotals(); //Refreshes the right-hand side with new information
+            }
+            else
+            {
+                MessageBox.Show("You must enter a name"); //You are an idiot
+            }
+        }
+
+        //Remove a participant
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (participantsList.SelectedIndex >= 0) //Makes sure you've selected someone
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to remove " + participantsList.Text + "?", "Remove Participant", MessageBoxButtons.YesNo); //prompts to make sure you really want to remove them.
+                if (dialogResult == DialogResult.Yes) //If your response was 'yes' then it runs below
+                {
+                    Globals.participants.RemoveAll(x => x.name == participantsList.Text); //Removes all existing tickets from global tickets list
+                    Globals.tickets.RemoveAll(x => x == participantsList.Text); //Removes all objects from participants object list where the name equals the selected name
+                    participantsList.Items.Remove(participantsList.Text); //Removes the name from the drop-down comboBox
+                    refreshTotals(); //Refreshes the right-hand side with new information
+                }
+            }
+        }        
+    }
+}
